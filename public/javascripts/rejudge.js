5fdd6f7d67bcff07ea09402297eec584d2d5f607
jiaaoHo <rivenhe@foxmail.com>
2016-12-04 17:31:12 +0800

    fixStatus

--- public/javascripts/rejudge.js
+++ public/javascripts/rejudge.js
@@ -1,5 +1,18 @@
 (function() {
    var RejudgeCtrl = [ '$scope', '$http', '$timeout', function($scope, $http, $timeout) {
+	   var emptyFilter = function() {
+		   var res = undefined;
+		   for (var i in $scope.filter) {
+			   res = res || $scope.filter[i];
+		   }
+		   return res === undefined;
+	   };
+	   var updateList = function(res) {
+		   if (res.data.data) {
+			   $scope.list = res.data.data;
+		   }
+		   $scope.lastUpdateTime = Date.now();
+	   };
        $scope.filter = {};
        $scope.list = [];
        $scope.updateList = function() {
@@ -8,11 +21,8 @@
 				   $scope.filter[i] = undefined;
 			   }
 		   }
-           $http.post('/api/status', { queryAttr: JSON.stringify($scope.filter) }).then(function(res) {
-               $scope.list = res.data.data.reverse();
-			   $scope.lastUpdateTime = Date.now();
-           });
-       };
+		   $http.post('/api/status', { queryAttr: JSON.stringify($scope.filter) }).then(updateList);
+	   };
        $scope.rejudge = function(_id) {
            $http.post('/api/rejudge', { _id: _id });
            $scope.updateList();
