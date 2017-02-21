882b7025f839f363b8f0f4401329126ae614c948
laekov <laekov.h@gmail.com>
2017-02-21 16:54:42 +0800

    statics

---
+++ oj/staticdata/cus.js
@@ -0,0 +1,30 @@
+var fs = require('fs');
+var data = {
+	title: 'Custom test',
+	langs: [ {
+		name: 'g++',
+		exec: 'g++',
+		args: '-DONLINE_JUDGE'
+	}, {
+		name: 'g++ with std11',
+		exec: 'g++',
+		args: '-std=c++11 -DONLINE_JUDGE'
+	}, {
+		name: 'gcc',
+		exec: 'gcc',
+		args: '-DONLINE_JUDGE'
+	}, {
+		name: 'pascal',
+		exec: 'fpc',
+		args: ''
+	} ], cases: [ {
+		ansId: 0,
+		score: 100,
+		inputFile: 'in',
+		outputFile: 'out',
+		time_limit: 5000,
+		mem_limit: 512,
+		spjPath: ''
+	} ],
+};
+fs.writeFile('cus.config.default', JSON.stringify(data));
